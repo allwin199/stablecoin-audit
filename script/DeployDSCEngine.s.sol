22ce094e8a31519b0f2b41f0e5ae0fdab0082e1e
worksofallwin <princeallwin199@gmail.com>
2023-08-09 18:12:44 +0530

    Start DeployScripts for DSCEngine

---
+++ script/DeployDSCEngine.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+
+contract DeployDSCEngine is Script {
+    DSCEngine dscEngine;
+    DecentralizedStableCoin dsCoin;
+
+    function run() external returns (DSCEngine) {
+        vm.startBroadcast();
+        dscEngine = new DSCEngine();
+        vm.stopBroadcast();
+        return dscEngine;
+    }
+}

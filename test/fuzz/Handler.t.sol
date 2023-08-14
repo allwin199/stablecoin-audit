82056d320b39b5ee07ffd78391dcf468826911cd
worksofallwin <princeallwin199@gmail.com>
2023-08-14 18:13:45 +0530

    complete invariant test for mintDSC

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 // Handler is going to narrow down the way we call function
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console2, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 
 import {DSCEngine} from "../../src/DSCEngine.sol";
@@ -19,6 +19,7 @@ contract Handler is Test {
     ERC20Mock wbtc;
 
     uint96 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
+    address[] public usersWithCollateralDeposited;
 
     constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsCoin) {
         dscEngine = _dscEngine;
@@ -38,6 +39,53 @@ contract Handler is Test {
         collateral.mint(msg.sender, amountCollateral);
         collateral.approve(address(dscEngine), amountCollateral);
         dscEngine.depositCollateral(address(collateral), amountCollateral);
+        usersWithCollateralDeposited.push(msg.sender);
+        vm.stopPrank();
+    }
+
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollaterlFromSeed(collateralSeed);
+
+        uint256 wethBalanceOfUser = dscEngine.getCollateralBalanceOfUser(msg.sender, address(weth));
+        // uint256 wbtcBalanceOfUser = dscEngine.getCollateralBalanceOfUser(msg.sender, address(wbtc));
+
+        uint256 maxCollateralToRedeem = wethBalanceOfUser;
+        // user can only redeem the max collateral deposited
+
+        amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+
+        if (amountCollateral == 0) {
+            return;
+        }
+
+        dscEngine.redeemCollateral(address(collateral), amountCollateral);
+    }
+
+    function mintDSC(uint256 amountDSCToMint, uint256 addressSeed) public {
+        uint256 usersLength = usersWithCollateralDeposited.length;
+        if (usersLength == 0) {
+            return;
+        }
+        uint256 userIndex = addressSeed % usersLength;
+        address sender = usersWithCollateralDeposited[userIndex];
+
+        (uint256 totalDSCMinted, uint256 collateralValueInUsd) = dscEngine.getAccountInformation(sender);
+
+        int256 maxDSCToMint = (int256(collateralValueInUsd) / 2) - int256(totalDSCMinted);
+
+        if (maxDSCToMint < 0) {
+            return;
+        }
+
+        amountDSCToMint = bound(amountDSCToMint, 0, uint256(maxDSCToMint));
+
+        if (amountDSCToMint == 0) {
+            console.log("amountDSCToMint", amountDSCToMint);
+            return;
+        }
+
+        vm.startPrank(sender);
+        dscEngine.mintDSC(MAX_DEPOSIT_SIZE);
         vm.stopPrank();
     }
 
--- test/fuzz/Invariants.t.sol
+++ test/fuzz/Invariants.t.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.8.18;
 // 1. The total supply of DSC should be less than the total value of collateral
 // 2. Getter view functions should never revert
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 
 import {Handler} from "./Handler.t.sol";
@@ -28,6 +28,7 @@ contract OpenInvariantsTest is StdInvariant, Test {
     address public ethUsdPriceFeed;
     address public btcUsdPriceFeed;
     uint256 public deployerKey;
+    Handler handler;
 
     address public user = address(1);
     uint256 public constant STARTING_USER_BALANCE = 10e18;
@@ -45,7 +46,7 @@ contract OpenInvariantsTest is StdInvariant, Test {
         // if we say target contract as dscEngine, then fuzz will call the functions randomly
         // for eg, redeemCollateral can be called before depositCollateral
         // so address this, we have to create a handler
-        Handler handler = new Handler(dscEngine, dsCoin);
+        handler = new Handler(dscEngine, dsCoin);
 
         targetContract(address(handler));
     }
@@ -60,6 +61,11 @@ contract OpenInvariantsTest is StdInvariant, Test {
 
         uint256 wethValue = dscEngine.getUsdValue(weth, totalWethDeposited);
         uint256 wbtcValue = dscEngine.getUsdValue(wbtc, totalWbtcDeposited);
+
+        console2.log("wethValue", wethValue);
+        console2.log("wbtcValue", wbtcValue);
+        console2.log("totalSupployOfDSC", totalSupployOfDSC);
+        console2.log("mintCalled", handler.timesMintIsCalled());
 
         assertGe(wethValue + wbtcValue, totalSupployOfDSC);
     }
